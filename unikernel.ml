75ea08ba9644454b24a6f5bf680c7c2319f7a0ae
Gabriel <g2p.code@gmail.com>
2019-03-08 15:08:54 +0100

    Move functions so they can use binfo.sector_size

--- unikernel.ml
+++ unikernel.ml
@@ -4,14 +4,6 @@ module Main (S: Mirage_stack_lwt.V4)(B: Mirage_types_lwt.BLOCK) = struct
 
   let align i n = (i + n - 1) / n * n
 
-  let alloc_sector len = Cstruct.create (align len 512)
-
-  let sector_of_int i =
-    let j = `Assoc [ ("magic", `Int 31337); ("counter", `Int i) ] in
-    let s = Yojson.Basic.to_string j in
-    let buf' = Cstruct.of_string ~allocator:alloc_sector s in
-    Cstruct.set_len buf' 512
-
   let int_of_sector buf =
     let s = Cstruct.to_string buf in
     let s' = String.sub s 0 (String.index s '\000') in
@@ -46,44 +38,48 @@ module Main (S: Mirage_stack_lwt.V4)(B: Mirage_types_lwt.BLOCK) = struct
     Logs.info (fun f -> f "[%a:%d] Closing connection"
                 Ipaddr.V4.pp dst dst_port)
   
-  let store_counter block counter flow =
-    let buf = sector_of_int counter in 
-    B.write block Int64.zero [ buf ] >>= function
-    | Error e -> log_block_write_err e; S.TCPV4.close flow
-    | Ok ()   -> log_closing flow; S.TCPV4.close flow
- 
-  let write_response block counter flow =
-    let counter' = counter + 1 in
-    S.TCPV4.write flow (cstruct_of_int counter') >>= function
-    | Error e -> log_write_err flow e; S.TCPV4.close flow
-    | Ok ()   -> store_counter block counter' flow
- 
-  let load_counter block flow =
-    let buf = alloc_sector 1 in
-    B.read block Int64.zero [ buf ] >>= function
-    | Error e -> log_block_read_err e; S.TCPV4.close flow
-    | Ok ()   -> write_response block (int_of_sector buf) flow
-
   let hello = Cstruct.of_string "Hello\n"
-
-  let start_request block flow =
-    log_new flow;
-    S.TCPV4.write flow hello >>= function
-    | Error e -> log_write_err flow e; S.TCPV4.close flow
-    | Ok ()   -> load_counter block flow
-  
-  let initialize block =
-    let buf = sector_of_int 0 in 
-    B.write block Int64.zero [ buf ] >>= function
-    | Error e -> log_block_write_err e; Lwt.return_unit
-    | Ok ()   -> (
-      Logs.info (fun m -> m "Successfully initialised block device");
-      Lwt.return_unit
-    )
 
   let start s b : unit Lwt.t =
     B.get_info b >>= fun binfo ->
     Logs.info (fun m -> m "Sector size %d" binfo.sector_size);
+
+    let rec alloc_sector len = Cstruct.create (align len binfo.sector_size)
+    and sector_of_int i =
+      let j = `Assoc [ ("magic", `Int 31337); ("counter", `Int i) ] in
+      let s = Yojson.Basic.to_string j in
+      let buf' = Cstruct.of_string ~allocator:alloc_sector s in
+      Cstruct.set_len buf' binfo.sector_size
+    and initialize block =
+      let buf = sector_of_int 0 in
+      B.write block Int64.zero [ buf ] >>= function
+      | Error e -> log_block_write_err e; Lwt.return_unit
+      | Ok ()   -> (
+        Logs.info (fun m -> m "Successfully initialised block device");
+        Lwt.return_unit
+      )
+    and store_counter block counter flow =
+      let buf = sector_of_int counter in 
+      B.write block Int64.zero [ buf ] >>= function
+      | Error e -> log_block_write_err e; S.TCPV4.close flow
+      | Ok ()   -> log_closing flow; S.TCPV4.close flow
+    and write_response block counter flow =
+      let counter' = counter + 1 in
+      S.TCPV4.write flow (cstruct_of_int counter') >>= function
+      | Error e -> log_write_err flow e; S.TCPV4.close flow
+      | Ok ()   -> store_counter block counter' flow
+    and load_counter block flow =
+      let buf = alloc_sector 1 in
+      B.read block Int64.zero [ buf ] >>= function
+      | Error e -> log_block_read_err e; S.TCPV4.close flow
+      | Ok ()   -> write_response block (int_of_sector buf) flow
+    and start_request block flow =
+      log_new flow;
+      S.TCPV4.write flow hello >>= function
+      | Error e -> log_write_err flow e; S.TCPV4.close flow
+      | Ok ()   -> load_counter block flow
+    in
+
     if Key_gen.init () then initialize b else (
       let port = Key_gen.port () in
       Logs.info (fun m -> m "Listening on [%a:%d]"
